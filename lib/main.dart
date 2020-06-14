8c4d120181367a4fb7ea4cf54851e494add05f2a
Mohammed Rashid k <mohammed1rashid786@gmail.com>
2020-06-14 14:02:50 +0530

    QuestionBank done

--- lib/main.dart
+++ lib/main.dart
@@ -31,16 +31,27 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Icon> scoreKeeper = [
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-  ];
+  List<Icon> scoreKeeper = [];
+
+  void checkAns(bool userAns) {
+    bool correctAnswer = questionBanks.questionAns();
+
+    setState(() {
+      if (userAns == correctAnswer) {
+        scoreKeeper.add(Icon(
+          Icons.check,
+          color: Colors.green,
+        ));
+      } else {
+        scoreKeeper.add(Icon(
+          Icons.close,
+          color: Colors.red,
+        ));
+      }
+
+      questionBanks.nextQuestion();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -78,12 +89,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnswer = questionBanks.questionAns();
-                if (correctAnswer == true) {
-                } else {}
-                setState(() {
-                  questionBanks.nextQuestion();
-                });
+                checkAns(true);
               },
             ),
           ),
@@ -101,12 +107,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnswer = questionBanks.questionAns();
-                if (correctAnswer == false) {
-                } else {}
-                setState(() {
-                  questionBanks.nextQuestion();
-                });
+                checkAns(false);
               },
             ),
           ),
